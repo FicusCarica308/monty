47acab15886b42d14cd0dd792de591bb5f50c037
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-22 23:06:12 +0000

    removed printf in file

--- get_bytecode.c
+++ get_bytecode.c
@@ -60,7 +60,6 @@ void get_bytecodes(char *file_name)
 	{
         op_code = get_code(line);
         stack_handler(op_code, line_num, &stack); /*new*/
-        printf("element: %s: value: %s:\n", op_code[0], op_code[1]);
         line_num++;
         free(op_code);
 	}
