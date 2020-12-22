3759981fbe8e4e0278a5a344388bf52265e33840
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-22 23:47:36 +0000

    file used to print errors

---
+++ errors.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+void invalid_error(unsigned int line_n, char **op_c, stack_t **stack, char *line, FILE *fd)
+{
+    fprintf(stderr, "L%u: unknown instruction %s\n", line_n, op_c[0]);
+    free(line);
+    free(op_c);
+    fclose(fd);
+    free_stack(&*stack);
+    exit(EXIT_FAILURE);
+}
