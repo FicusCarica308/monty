6137b1ff27e601c5be3e2d4c2abc0690d834f755
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-23 03:49:50 +0000

    added betty code

--- stack_handler.c
+++ stack_handler.c
@@ -1,64 +1,75 @@
 #include "monty.h"
+/**
+ *atoi_check - checks if atoi is a number/is not null
+ *@num: string with number
+ *Return: returns the number or -1 on failure
+ */
 int atoi_check(char *num)
 {
-        if (num == NULL)
-                return (-1);
-        if ((atoi(num) == 0 && strcmp(num, "0") != 0))
-                return (-1);
-        else
-                return (atoi(num));
+	if (num == NULL)
+		return (-1);
+	if ((atoi(num) == 0 && strcmp(num, "0") != 0))
+		return (-1);
+	else
+		return (atoi(num));
 }
 /**
  *get_op_func - will find which function to return depending on a given
  * operator
- *@s: this is the given operator character
+ *@op_code: the current given opcode on the stack
  *Return: returns a pointer to the needed operation
  */
- void (*get_op_func(char *op_code))(stack_t **, unsigned int)
+void (*get_op_func(char *op_code))(stack_t **, unsigned int)
 {
-        instruction_t ops[] = {
-                {"push", op_push},
-                {"pall", op_pall},
-                {"pint", op_pint},
-                {"pop", op_pop},
-                {"add", op_add},
-                {"swap", op_swap},
-                {NULL, NULL}
-        };
+	instruction_t ops[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"add", op_add},
+		{"swap", op_swap},
+		{NULL, NULL}
+	};
 
-        int i = 0;
+	int i = 0;
 
-        while (ops[i].opcode != NULL)
-        {
-                if (strcmp(op_code, ops[i].opcode) == 0)
-                        return (ops[i].f);
-                i++;
-        }
-        return (NULL);
+	while (ops[i].opcode != NULL)
+	{
+		if (strcmp(op_code, ops[i].opcode) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
+/**
+ *stack_handler - handler all stack operations and op_code operations
+ *@op_code: current op_code on the stack
+ *@line_n: the current line number
+ *@stack: the head of the stack list
+ *Return: returns negative numbers if failure occurs and 0 if not
+ */
 int stack_handler(char **op_code, unsigned int line_n, stack_t **stack)
 {
-    void (*code_func)(stack_t **, unsigned int);
+	void (*code_func)(stack_t **, unsigned int);
 
+	if (strcmp(op_code[0], "push") == 0)
+	{
+		if (atoi_check(op_code[1]) != -1)
+			push_value = atoi(op_code[1]);
+		else
+/*-2 stands for push error*/
+			return (-2);
+	}
+	if (strcmp(op_code[0], "nop") == 0)
+		return (0);
+	code_func = get_op_func(op_code[0]);
 
-    if (strcmp(op_code[0], "push") == 0)
-    {
-            if (atoi_check(op_code[1]) != -1)
-                push_value = atoi(op_code[1]);
-        else
-                /*-2 stands for push error*/
-                return (-2);
-    }
-    if (strcmp(op_code[0], "nop") == 0)
-        return (0);
-    code_func = get_op_func(op_code[0]);
-
-    if (code_func == NULL)
-        /*-3 stands for not a op_code error*/
-        return (-3);
-    (*code_func)(&*stack, line_n);
-    if (push_value == -1)
-    /*-1 id everything else*/
-        return (-1);
-    return (0);
+	if (code_func == NULL)
+		/*-3 stands for not a op_code error*/
+		return (-3);
+	(*code_func)(&*stack, line_n);
+	if (push_value == -1)
+		/*-1 id everything else*/
+		return (-1);
+	return (0);
 }
