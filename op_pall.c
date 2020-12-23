4434ba10d5b3e5026884ce673e9ead13f16e0589
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-23 03:28:30 +0000

    betty code

--- op_pall.c
+++ op_pall.c
@@ -1,14 +1,20 @@
 #include "monty.h"
+/**
+ *op_pall - The opcode pall prints all the values on the stack,
+ *starting from the top of the stack.
+ *@stack: head to current stack
+ *@line_number: current line number
+ */
 void op_pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp_head;
-    (void)line_number;
+	stack_t *temp_head;
+	(void)line_number;
 
-     temp_head = *stack;
+	temp_head = *stack;
 
-    while (temp_head != NULL)
-    {
-        printf("%d\n", temp_head->n);
-        temp_head = temp_head->next;
-    }
+	while (temp_head != NULL)
+	{
+		printf("%d\n", temp_head->n);
+		temp_head = temp_head->next;
+	}
 }
