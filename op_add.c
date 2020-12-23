55e1b60e848349ca1aea81aab3b97e97c152faf7
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-23 03:25:41 +0000

    betty code

--- op_add.c
+++ op_add.c
@@ -1,17 +1,22 @@
 #include "monty.h"
+/**
+ *op_add - The opcode add adds the top two elements of the stack
+ *@stack: head to current stack
+ *@line_number: current line number
+ */
 void op_add(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp_head;
+	stack_t *temp_head;
 
-    temp_head = *stack;
-    if (temp_head == NULL || temp_head->next == NULL)
-    {
-        dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number);
-        push_value = -1;
-    }
-    else
-    {
-        temp_head->next->n = temp_head->n + temp_head->next->n;
-        op_pop(&*stack, line_number);
-    }
+	temp_head = *stack;
+	if (temp_head == NULL || temp_head->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number);
+		push_value = -1;
+	}
+	else
+	{
+		temp_head->next->n = temp_head->n + temp_head->next->n;
+		op_pop(&*stack, line_number);
+	}
 }
