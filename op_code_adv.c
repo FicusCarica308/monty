700b72f77962c1c7cf76eeaea870b276df2e6ed1
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-12-23 20:23:31 +0000

    fixing some output errors with op_code_adv.c

--- op_code_adv.c
+++ op_code_adv.c
@@ -9,20 +9,20 @@ void op_pchar(stack_t **stack, unsigned int line_number)
 
         if (temp_head == NULL)
         {
-		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
-		push_value = -1;
+            fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+            push_value = -1;
         }
         else
         {
-		if (temp_head-> n > 127 || temp_head->n < 0)
-		{
-			fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
-			push_value = -1;
-		}
-		else
-		{
-			printf("%c\n", temp_head->n);
-		}
+            if (temp_head-> n > 127 || temp_head->n < 0)
+            {
+                fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+                push_value = -1;
+            }
+            else
+            {
+                printf("%c\n", temp_head->n);
+            }
         }
 }
 /**
@@ -31,24 +31,24 @@ void op_pchar(stack_t **stack, unsigned int line_number)
 */
 void op_pstr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp_head = *stack;
-	(void)line_number;
+    stack_t *temp_head = *stack;
+    (void)line_number;
 
 	if (temp_head == NULL)
-		putchar('\n');
-	else
-	{
-		while (temp_head != NULL)
-		{
-			if (temp_head-> n > 127 || temp_head->n <= 0)
-			{
-				putchar('\n');
-				break;
-			}
-			printf("%c", temp_head->n);
-            if (temp_head != 0)
+        putchar('\n');
+    else
+    {
+        while (temp_head != NULL)
+	    {
+            if (temp_head-> n > 127 || temp_head->n <= 0)
+                {
                 putchar('\n');
-			temp_head = temp_head->next;
-		}
-	}
+                break;
+                }
+            printf("%c", temp_head->n);
+		    temp_head = temp_head->next;
+	    }
+        if (temp_head->prev->n != 0)
+            putchar('\n');
+    }
 }
